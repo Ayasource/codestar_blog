90d7fd625abc82101d2d028717f90c4e088bcc45
ayasource <ayana-aac@hotmail.co.uk>
2026-01-05 16:00:52 +0000

    Updating settings

--- codestart/settings.py
+++ codestart/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&+%*-%qdep$j%9(x=c*ijw!oe5ebrd35d-ueby(xv1$e0f6)+v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [,'.herokuapp.com','127.0.0.1']
 
 
 # Application definition
