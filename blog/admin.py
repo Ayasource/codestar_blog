0bb5da9d55e4f6dcab9d5c7958d1582b81cf413f
ayasource <ayana-aac@hotmail.co.uk>
2026-01-07 09:24:13 +0000

    Add meta data

--- blog/admin.py
+++ blog/admin.py
@@ -4,3 +4,8 @@ from .models import Post, Comment
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
+class Meta:
+        ordering = ["-created_on"]
+
+def __str__(self):
+    return f"{self.title} | written by {self.author}"       
